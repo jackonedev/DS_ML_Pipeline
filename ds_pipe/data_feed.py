d58a045d692282be695a0ede21119a0ed6d114ad
jackonedev <jackone.action.software@gmail.com>
2024-07-27 10:46:52 -0300

    updated ds_pipe/data_feed.py: still in develop

--- ds_pipe/data_feed.py
+++ ds_pipe/data_feed.py
@@ -7,4 +7,21 @@ Actualiza los tipos de datos si es necesario.
 Descarga una copia en formato parquet.
 """
 
-from utils.config import PROJECT_ROOT
+import os
+
+import pandas as pd
+
+from utils.config import DATASETS_PATH, PARQUET_PATH
+
+
+def main_feed():
+    # File name
+    file_name = os.path.join(DATASETS_PATH, "challenge_edMachina.csv")
+    # Read data
+    df = pd.read_csv(file_name, sep=';')
+    # Check data types
+    print(df.dtypes)
+    # Check for date columns
+    # print(df.select_dtypes(include=["datetime64"]).columns)
+    # Save a copy in parquet format
+    df.to_parquet(f"{PARQUET_PATH}/covid_data.parquet")
