1fce22a23603f394e2d76124812fa83e44c8b556
jackonedev <jackone.action.software@gmail.com>
2024-07-27 18:19:15 -0300

    updated main and ds_pipe

--- ds_pipe/data_feed.py
+++ ds_pipe/data_feed.py
@@ -11,17 +11,35 @@ import os
 
 import pandas as pd
 
+
 from utils.config import DATASETS_PATH, PARQUET_PATH
 
 
+
 def main_feed():
-    # File name
+
+    # Read Local CSV file
     file_name = os.path.join(DATASETS_PATH, "challenge_edMachina.csv")
-    # Read data
-    df = pd.read_csv(file_name, sep=';')
-    # Check data types
-    print(df.dtypes)
-    # Check for date columns
-    # print(df.select_dtypes(include=["datetime64"]).columns)
+    df = pd.read_csv(file_name, sep=";")
+
+    # Convert to datetime the columns with 'at' in the name
+    # The 'fecha_mesa_epcoh' column also,
+    # Update the UTC to Buenos Aires timezone
+    date_sample = df[
+        df.columns[df.columns.str.endswith("at")].to_list() + ["fecha_mesa_epoch"]
+    ]
+    datetime_values = (date_sample.astype(int, errors="ignore") * 1e9).apply(
+        pd.to_datetime
+    )
+    datetime_values_utc = datetime_values.apply(
+        lambda col: col.dt.tz_localize("UTC").dt.tz_convert("America/Buenos_Aires")
+    )
+    # FutureWarning: Setting an item of incompatible dtype is deprecated
+    df.update(datetime_values_utc)
+    
+    # Rest of the columns
+    non_date_sample = df[df.columns[~df.columns.isin(date_sample.columns)]]
+
+    # Not Implemented ... yet
     # Save a copy in parquet format
-    df.to_parquet(f"{PARQUET_PATH}/covid_data.parquet")
+    # df.to_parquet(f"{PARQUET_PATH}/covid_data.parquet")
