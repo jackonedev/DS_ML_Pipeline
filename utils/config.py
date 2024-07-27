25bf3c82fe0f7fb7f08843be8601422988c71a0b
jackonedev <jackone.action.software@gmail.com>
2024-07-27 10:45:18 -0300

    utils/config: added environment variables

--- utils/config.py
+++ utils/config.py
@@ -14,3 +14,9 @@ if not os.path.exists(os.path.join(PROJECT_ROOT, "datasets", "samples")):
 
 if not os.path.exists(os.path.join(PROJECT_ROOT, "features")):
     os.makedirs(os.path.join(PROJECT_ROOT, "features"))
+
+
+DATASETS_PATH = os.path.join(PROJECT_ROOT, "datasets")
+PARQUET_PATH = os.path.join(DATASETS_PATH, "parquet")
+SAMPLES_PATH = os.path.join(DATASETS_PATH, "samples")
+FEATURES_PATH = os.path.join(PROJECT_ROOT, "features")
