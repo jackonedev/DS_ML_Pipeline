185554b3baba3e704f6245da5df70cdf0f58b88b
jackonedev <jackone.action.software@gmail.com>
2024-07-27 18:18:13 -0300

    updated requirements.txt created tools.dates

---
+++ tools/dates.py
@@ -0,0 +1,23 @@
+import pendulum
+
+def format_date(date_str: str, language: str = 'es') -> str:
+    """
+    Formats a date string into a humanized format based on the given language.
+
+    Parameters:
+        date_str (str): The date string to format.
+        language (str, optional): The language code for the desired format. Defaults to 'es'.
+
+    Returns:
+        str: The formatted date string.
+
+    Example:
+        >>> format_date('2022-01-01', 'en')
+        'Saturday 01 January 2022'
+        
+        >>> pd.Series([...]).apply(format_date)
+        pd.Series([...])
+    """
+    dt = pendulum.parse(date_str)
+    pendulum.set_locale(language)
+    return dt.format('dddd DD MMMM YYYY')
