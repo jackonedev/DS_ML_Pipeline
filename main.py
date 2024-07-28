b83542aecda59f0cf82d6ec8565632003392ad29
jackonedev <jackone.action.software@gmail.com>
2024-07-28 09:52:36 -0300

    updated main pipe

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python
 
-from ds_pipe.data_feed import *
+from ds_pipe.data_feed import main_feed
+from ds_pipe.data_preprocessing import hello
+
+if __name__ == "__main__":
+    main_feed("challenge_edMachina.csv")
+    hello()
