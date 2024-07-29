8ba4743e2b95f32a7ace1381a9f0ba161836b088
jackonedev <jackone.action.software@gmail.com>
2024-07-29 09:59:57 -0300

    pylint: disable part of code inaccessible

--- tools/eda.py
+++ tools/eda.py
@@ -1,7 +1,7 @@
 import os
 
-from ydata_profiling import ProfileReport
 import pandas as pd
+from ydata_profiling import ProfileReport
 
 
 def data_info(data: pd.DataFrame, sort=False) -> pd.DataFrame:
@@ -32,14 +32,14 @@ def automatic_report(df: pd.DataFrame, title: str, download: bool = False) -> di
     # el output_file va a ser el df.name, si no existe se asigna un nombre por default
     # TODO: crear un directory "reports" y configurarlo en utils.config
     profile = ProfileReport(df, title=title)
-    
+
+    # pylint: disable=W0101
     if download:
         raise NotImplementedError("Download option is not implemented yet")
         output_file = df.name if df.name else "report.html"
         # TODO: terminar de configuar el path de descarga con utils.config
         output_file = os.path.abspath(output_file)
         profile.to_file(output_file)
-    
+
     # return profile.to_json()
     return profile
-
