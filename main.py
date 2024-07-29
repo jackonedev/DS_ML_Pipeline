4443fa6761addc21e4829b30be73789d8e105c11
jackonedev <jackone.action.software@gmail.com>
2024-07-29 20:51:36 -0300

    updated main pipeline

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import pendulum
+
 from ds_pipe.data_feed import main_feed
 from ds_pipe.data_preprocessing import data_preprocessing
 
@@ -12,3 +14,10 @@ if __name__ == "__main__":
     main_feed(f"{file_name}.csv")
     # TODO: descargar un reporte de los datos
     data_preprocessing(f"{file_name}.parquet")
+
+    current_time = (
+        pendulum.now().set(microsecond=0, second=0).format("dddd, MMMM Do YYYY, h:mm A")
+    )
+
+    print("Current Execution Time:\n", current_time, end="\n\n")
+    print("  Main Pipeline Completed Successfully  ".center(88, "."), end="\n\n")
