78584f84e6344cb2d9e86d9bd51abfbef2ca8e63
jackonedev <jackone.action.software@gmail.com>
2024-07-29 21:07:19 -0300

    updated main pipeline: ahora es una funcion que tiene un return

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
+import pandas as pd
 import pendulum
 
 from ds_pipe.data_feed import main_feed
 from ds_pipe.data_preprocessing import data_preprocessing
+from utils.config import PARQUET_PATH
 
-# TODO: configurar utils.config - añadir reportes
 
-
-if __name__ == "__main__":
+def main():
 
     file_name = "challenge_edMachina"
     main_feed(f"{file_name}.csv")
@@ -19,5 +19,14 @@ if __name__ == "__main__":
         pendulum.now().set(microsecond=0, second=0).format("dddd, MMMM Do YYYY, h:mm A")
     )
 
+    # Buscar la version actual del dataset
+    df = pd.read_parquet(f"{PARQUET_PATH}/{file_name}_cleaned.parquet")
+
     print("Current Execution Time:\n", current_time, end="\n\n")
     print("  Main Pipeline Completed Successfully  ".center(88, "."), end="\n\n")
+
+    return df
+
+
+if __name__ == "__main__":
+    main()
