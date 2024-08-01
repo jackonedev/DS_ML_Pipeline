75b956918a51eeddefb7b02bb5db661a481965ac
jackonedev <jackone.action.software@gmail.com>
2024-08-01 19:51:57 -0300

    updated main pipeline

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
 #!/usr/bin/env python
+import time
 
-import pandas as pd
 import pendulum
 
 from ds_pipe.data_feed import main_feed
 from ds_pipe.data_preprocessing import data_preprocessing
-from utils.config import PARQUET_PATH
+from ds_pipe.data_research import main_research
+from ds_pipe.data_results import main_results
 
 
 def main():
-
+    start = time.time()
     file_name = "challenge_edMachina"
     main_feed(f"{file_name}.csv")
     # TODO: descargar un reporte de los datos
     data_preprocessing(f"{file_name}.parquet")
-
+    main_research()
+    main_results(f"{file_name}_cleaned.parquet")
     current_time = (
         pendulum.now().set(microsecond=0, second=0).format("dddd, MMMM Do YYYY, h:mm A")
     )
-
-    # Buscar la version actual del dataset
-    df = pd.read_parquet(f"{PARQUET_PATH}/{file_name}_cleaned.parquet")
-
+    end = time.time()
     print("Current Execution Time:\n", current_time, end="\n\n")
+    print("Total Execution Time:\n", round(end - start, 2), "seconds", end="\n\n")
     print("  Main Pipeline Completed Successfully  ".center(88, "."), end="\n\n")
-
-    return df
 
 
 if __name__ == "__main__":
