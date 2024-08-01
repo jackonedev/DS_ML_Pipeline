9f66c812c6b5c85f3f6a34cacb0866573ead1155
jackonedev <jackone.action.software@gmail.com>
2024-08-01 19:51:06 -0300

    added mock ds_pipe step: data research

--- ds_pipe/data_research.py
+++ ds_pipe/data_research.py
@@ -11,3 +11,9 @@ La ejecución de este step:
     
 El objetivo es crear el material para utilizar luego en jupyter notebook.
 """
+
+
+def main_research():
+    "Not Implemented"
+    print("  Step 3: Data Research Started  ".center(88, "."), end="\n\n")
+    print("  Step 3: Data Research Completed  ".center(88, "."), end="\n\n")
