607c8083e4a5fcc8e7c350275eeb8635eddee9f1
jackonedev <jackone.action.software@gmail.com>
2024-08-03 17:52:16 -0300

    added functions to tools/ml

--- tools/ml.py
+++ tools/ml.py
@@ -1,5 +1,8 @@
+import ast
 import os
+from typing import List
 
+import mlflow
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
@@ -28,3 +31,29 @@ def preprocess_data(df, test_size, random_state, scaler):
     X_test_scaled = scaler.transform(X_test)
 
     return X_train_scaled, X_test_scaled, y_train, y_test
+
+
+def train_model(X_train, y_train, model, **kwargs):
+    model = model(**kwargs)
+    model.fit(X_train, y_train)
+    return model
+
+
+def obtain_model_1_features() -> List[str]:
+    experiments = mlflow.search_runs(experiment_ids=["1"], search_all_experiments=True)
+    score_cols = ["metrics.train", "metrics.test"]
+    cols_name = []
+    for col in experiments.columns:
+        if col.startswith(score_cols[0]) or col.startswith(score_cols[1]):
+            cols_name.append(col)
+    run_max_score = experiments[["run_id"] + cols_name].max(axis=1, numeric_only=True)
+    id_max_score = run_max_score.idxmax()
+    # run_scores = experiments.iloc[id_max_score][cols_name]
+    feature_selected = ast.literal_eval(
+        experiments.iloc[id_max_score][["params.features_selected"]].values[0]
+    )
+    feature_selected_ = [f"metrics.coef_{ft}" for ft in feature_selected]
+    # se limpian las columnas con coeficiente de regularización Lasso igual a cero
+    coef_cero_mask = (experiments.iloc[id_max_score][feature_selected_] == 0).values
+    final_features = pd.Series(feature_selected)[~coef_cero_mask].values.tolist()
+    return final_features
