582fa3af7750b42c9045a445ae9ad9175b1a72d0
jackonedev <jackone.action.software@gmail.com>
2024-08-03 17:06:23 -0300

    updated ML main pipeline

--- main_ml.py
+++ main_ml.py
@@ -5,15 +5,18 @@ import warnings
 import pendulum
 
 from ml_pipe.ml_model_1_feature_selector import main_feature_selector
+from ml_pipe.ml_model_2_random_forest import main_random_forest
 
 warnings.filterwarnings("ignore")
+
 
 def main_ml():
     start = time.time()
     dataset_name = "challenge_edMachina"
     feature_name = "grouped_features"
-    main_feature_selector(dataset_name, feature_name)
-    
+    # main_feature_selector(dataset_name, feature_name)
+    main_random_forest(dataset_name, feature_name)
+
     # Execution Time
     current_time = (
         pendulum.now().set(microsecond=0, second=0).format("dddd, MMMM Do YYYY, h:mm A")
