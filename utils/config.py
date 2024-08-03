e99c69df95d46605a7cc801addc8428dab892050
jackonedev <jackone.action.software@gmail.com>
2024-08-03 15:09:11 -0300

    updated utils/config con el directorio de artifacts

--- utils/config.py
+++ utils/config.py
@@ -1,25 +1,29 @@
 import os
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATASETS_PATH = os.path.join(PROJECT_ROOT, "datasets")
 
 if not os.path.exists(os.path.join(PROJECT_ROOT, "datasets")):
     os.makedirs(os.path.join(PROJECT_ROOT, "datasets"))
 
-if not os.path.exists(os.path.join(PROJECT_ROOT, "datasets", "parquet")):
-    os.makedirs(os.path.join(PROJECT_ROOT, "datasets", "parquet"))
+if not os.path.exists(os.path.join(DATASETS_PATH, "parquet")):
+    os.makedirs(os.path.join(DATASETS_PATH, "parquet"))
 
-if not os.path.exists(os.path.join(PROJECT_ROOT, "datasets", "samples")):
-    os.makedirs(os.path.join(PROJECT_ROOT, "datasets", "samples"))
-
-if not os.path.exists(os.path.join(PROJECT_ROOT, "features")):
-    os.makedirs(os.path.join(PROJECT_ROOT, "features"))
+if not os.path.exists(os.path.join(DATASETS_PATH, "samples")):
+    os.makedirs(os.path.join(DATASETS_PATH, "samples"))
 
 if not os.path.exists(os.path.join(PROJECT_ROOT, "reports")):
     os.makedirs(os.path.join(PROJECT_ROOT, "reports"))
 
+if not os.path.exists(os.path.join(PROJECT_ROOT, "features")):
+    os.makedirs(os.path.join(PROJECT_ROOT, "features"))
 
-DATASETS_PATH = os.path.join(PROJECT_ROOT, "datasets")
+if not os.path.exists(os.path.join(PROJECT_ROOT, "mlartifacts")):
+    os.makedirs(os.path.join(PROJECT_ROOT, "mlartifacts"))
+
+
 PARQUET_PATH = os.path.join(DATASETS_PATH, "parquet")
 SAMPLES_PATH = os.path.join(DATASETS_PATH, "samples")
-FEATURES_PATH = os.path.join(PROJECT_ROOT, "features")
 REPORTS_PATH = os.path.join(PROJECT_ROOT, "reports")
+FEATURES_PATH = os.path.join(PROJECT_ROOT, "features")
+ARTIFACTS_PATH = os.path.join(PROJECT_ROOT, "mlartifacts")
