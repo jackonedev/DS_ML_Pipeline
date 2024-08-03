fd88dc4ab69a566467ec7a2adeee210ea52c8fb2
jackonedev <jackone.action.software@gmail.com>
2024-08-03 18:57:59 -0300

    updated utils/config: variables for downloading dataset

--- utils/config.py
+++ utils/config.py
@@ -1,7 +1,19 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATASETS_PATH = os.path.join(PROJECT_ROOT, "datasets")
+DATASET_NAME = "challenge_edMachina"
+
+# Google Drive Download Variable Environment
+SHARED_URL = os.getenv("SHARED_URL")
+FILE_ID = SHARED_URL.split("/")[-2]
+DOWNLOAD_URL = f"https://drive.google.com/uc?id={FILE_ID}"
+DOWNLOAD_PATH = os.path.join(DATASETS_PATH, DATASET_NAME + ".csv")
 
 if not os.path.exists(os.path.join(PROJECT_ROOT, "datasets")):
     os.makedirs(os.path.join(PROJECT_ROOT, "datasets"))
